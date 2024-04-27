0056cfdf084170c53a6bef85090cc43b903dc8ed
bart-ziolkowski <bart_ziolkowski@icloud.com>
2024-04-27 11:30:10 +0200

    change env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://localhost:3000",
+    API_URL: "https://hosteldk.vercel.app",
+    SMTP_PASSWORD: "0516fde7a867e3",
+    SMTP_PORT: 2525,
+    MAPBOX_ACCESS_TOKEN:
+      "pk.eyJ1IjoiYmFydC16aW9sa293c2tpIiwiYSI6ImNsdjlpbWJ5eTBoNGoyaXA4MGNwbW9xeDEifQ.Ag5FV6LEfCsYptZOdZQJrA",
+    CLOUDINARY_API_KEY: 177238823964242,
+    DB_URI:
+      "mongodb+srv://bart-ziolkowski:Meybiksong2016@@hosteldk.pftwzmb.mongodb.net/hosteldk?retryWrites=true&w=majority&appName=hosteldk",
+    SMTP_FROM_EMAIL: "noreply@hosteldk.dk",
+    SMTP_HOST: "sandbox.smtp.mailtrap.io",
+    CLOUDINARY_API_SECRET: "GLsXhOLw6hkmAkBaUG-JTI3-ne8",
+    NEXTAUTH_URL: "https://hosteldk.vercel.app",
   },
   images: {
     remotePatterns: [
