432eb0337de9811e15ad0d58f65d9d0858c82fda
bart-ziolkowski <bart_ziolkowski@icloud.com>
2024-04-27 21:06:14 +0200

    fix mapbox

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
     DB_URI:
       "mongodb+srv://vercel-admin-user:pNJT8inUdHj3uoeM@hosteldk.pftwzmb.mongodb.net/hosteldk?retryWrites=true&w=majority",
     NEXTAUTH_URL: "https://hosteldk.vercel.app",
+    MAPBOX_ACCESS_TOKEN:
+      "pk.eyJ1IjoiYmFydC16aW9sa293c2tpIiwiYSI6ImNsdjlpbWJ5eTBoNGoyaXA4MGNwbW9xeDEifQ.Ag5FV6LEfCsYptZOdZQJrA",
   },
   images: {
     remotePatterns: [
