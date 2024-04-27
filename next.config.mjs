b6c277989b2f6be37828bdfc3d1151d3fc1544f3
bart-ziolkowski <bart_ziolkowski@icloud.com>
2024-04-27 20:59:07 +0200

    changed env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {},
+  env: {
+    API_URL: "https://hosteldk.vercel.app",
+    DB_URI:
+      "mongodb+srv://vercel-admin-user:pNJT8inUdHj3uoeM@hosteldk.pftwzmb.mongodb.net/hosteldk?retryWrites=true&w=majority",
+    NEXTAUTH_URL: "https://hosteldk.vercel.app",
+  },
   images: {
     remotePatterns: [
       {
